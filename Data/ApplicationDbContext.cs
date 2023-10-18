0b5a8aa9a1a2763a8aa66b15c393bca54364df0e
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-10-18 17:39:01 +0800

    create the login function v2

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Accounting_System.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Accounting_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Accounting_System.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
