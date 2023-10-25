00c56f9aea89897a5f653c09d62b6ea2aa8ea725
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-10-25 17:55:01 +0800

    completed edit and delete function

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,6 +1,8 @@
 ﻿using Accounting_System.Data;
+using Accounting_System.Models;
 using Accounting_System.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Accounting_System.Controllers
 {
@@ -21,6 +23,111 @@ namespace Accounting_System.Controllers
             var customer = await _customerRepo.GetCustomersAsync();
 
             return View(customer);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Customer());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var customerType = customer.CustomerType;
+
+                customer.CustomerType = customerType.Replace("_", " ");
+
+                _dbContext.Add(customer);
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "The information you submitted is not valid!");
+                return View(customer);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _customerRepo.FindCustomerAsync(id);
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dbContext.Update(customer);
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_customerRepo.CustomerExist(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _customerRepo.FindCustomerAsync(id);
+
+            return View(customer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_dbContext.Customers == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Customers'  is null.");
+            }
+
+            var customer = await _dbContext.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _dbContext.Customers.Remove(customer);
+            }
+
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
