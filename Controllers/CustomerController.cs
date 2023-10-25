c38761b8febb1a3917803b1c00ae0d3d4e4039cd
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-10-25 12:34:32 +0800

    create customer master file

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,26 @@
+﻿using Accounting_System.Data;
+using Accounting_System.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting_System.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        private readonly CustomerRepo _customerRepo;
+
+        public CustomerController(ApplicationDbContext dbContext, CustomerRepo customerRepo)
+        {
+            _dbContext = dbContext;
+            _customerRepo = customerRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customer = await _customerRepo.GetCustomersAsync();
+
+            return View(customer);
+        }
+    }
+}
