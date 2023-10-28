80b93b3f81071a3d726cf3816e98e0cf61ae55c0
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-10-28 11:37:40 +0800

    add new accounts

---
+++ Migrations/20231028030510_add new field in chart of accounts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnewfieldinchartofaccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Parent",
+                table: "ChartOfAccounts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Parent",
+                table: "ChartOfAccounts");
+        }
+    }
+}
