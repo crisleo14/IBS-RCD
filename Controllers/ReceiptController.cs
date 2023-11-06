110161f860c537a2b0a17e1e864c5c9032df5a6c
crisleo14 <127143593+crisleo14@users.noreply.github.com>
2023-11-06 12:44:50 +0800

    Module/receipt (#91)
    * Make the zero in print invoice to blank
    
    * minor revision in CAS
    
    * implement to other user show status the other user created invoice
    
    * create official and collection receipt MVC
    
    * create official and collection receipt MVC

---
+++ Controllers/ReceiptController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting_System.Controllers
+{
+    public class ReceiptController : Controller
+    {
+        public IActionResult CollectionReceipt()
+        {
+            return View();
+        }
+        public IActionResult OfficialReceipt()
+        {
+            return View();
+        }
+    }
+}
