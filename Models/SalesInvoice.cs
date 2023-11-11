7b7f0100aeda4bfa9f8f4c86ee4ed6a47e9dac3d
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-11-11 13:30:24 +0800

    add status field and migrate to database

--- Models/SalesInvoice.cs
+++ Models/SalesInvoice.cs
@@ -96,5 +96,7 @@ namespace Accounting_System.Models
         [Display(Name = "VAT Amount")]
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
         public decimal VatAmount { get; set; }
+
+        public string Status { get; set; }
     }
 }
