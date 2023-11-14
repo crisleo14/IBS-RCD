3a34eeb70259cdac8178b22e41c51adcf14844a4
Leonard <andulanleonard@gmail.com>
2023-11-14 16:44:52 +0800

    create table for official receipt

---
+++ Models/OfficialReceipt.cs
@@ -0,0 +1,29 @@
+﻿namespace Accounting_System.Models
+{
+    public class OfficialReceipt : BaseEntity
+    {
+        public int SOAId { get; set; }
+
+        public string ORNo { get; set; }
+
+        public string Date { get; set; }
+
+        public string ReferenceNo { get; set; }
+
+        public string FormOfPayment { get; set; }
+
+        public int CheckNo { get; set; }
+
+        public string CheckDate { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public int SOADate { get; set; }
+
+        public string SOANo { get; set; }
+
+        public decimal SOAAmount { get; set; }
+
+        public bool IsPrint { get; set; }
+    }
+}
