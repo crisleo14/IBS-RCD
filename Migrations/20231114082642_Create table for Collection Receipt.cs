22377b480bcc1e7d40e84a70bd1d122202cebee1
Leonard <andulanleonard@gmail.com>
2023-11-14 16:30:05 +0800

    create table for collection receipt

---
+++ Migrations/20231114082642_Create table for Collection Receipt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatetableforCollectionReceipt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CollectionReceipt",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SalesInvoiceId = table.Column<int>(type: "integer", nullable: false),
+                    CRNo = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: false),
+                    ReferenceNo = table.Column<string>(type: "text", nullable: false),
+                    FormOfPayment = table.Column<string>(type: "text", nullable: false),
+                    CheckDate = table.Column<string>(type: "text", nullable: false),
+                    Check = table.Column<int>(type: "integer", nullable: false),
+                    Bank = table.Column<string>(type: "text", nullable: false),
+                    Branch = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    EWT = table.Column<decimal>(type: "numeric", nullable: false),
+                    Total = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsPrint = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollectionReceipt", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollectionReceipt");
+        }
+    }
+}
---
+++ Models/CollectionReceipt.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Accounting_System.Models
+{
+    public class CollectionReceipt : BaseEntity
+    {
+        public int SalesInvoiceId { get; set; }
+
+        public string CRNo { get; set; }
+
+        public string Date { get; set; }
+
+        public string ReferenceNo { get; set; }
+
+        public string FormOfPayment { get; set; }
+
+        public string CheckDate { get; set; }
+
+        public int Check { get; set; }
+
+        public string Bank { get; set; }
+
+        public string Branch { get; set;}
+
+        public decimal Amount { get; set;}
+
+        public decimal EWT { get; set;}
+
+        public decimal Total { get; set;}
+
+        public bool IsPrint { get; set;}
+    }
+}
