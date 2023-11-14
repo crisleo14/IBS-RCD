1c52543812beacbf106348fd6c93d3f0e364b7d4
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-11-14 15:45:50 +0800

    initial create of statement of account

---
+++ Controllers/StatementOfAccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting_System.Controllers
+{
+    public class StatementOfAccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
