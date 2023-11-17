34e25f9c03e70c106864a997b84d09539d7cb127
Leonard <andulanleonard@gmail.com>
2023-11-17 11:06:21 +0800

    Create static print of purchase order

---
+++ Controllers/PurchaseOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting_System.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
