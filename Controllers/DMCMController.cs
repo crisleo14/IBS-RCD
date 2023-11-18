7cd1b7026832ead299c9112666b60a8447c4ac05
Leonard <andulanleonard@gmail.com>
2023-11-18 17:02:43 +0800

    Create DMCM static template

---
+++ Controllers/DMCMController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting_System.Controllers
+{
+    public class DMCMController : Controller
+    {
+        public IActionResult DebitMemo()
+        {
+            return View();
+        }
+
+        public IActionResult CreditMemo()
+        {
+            return View();
+        }
+    }
+}
