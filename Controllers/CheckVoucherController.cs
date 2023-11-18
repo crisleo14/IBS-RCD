a99a05319d20b40f0b450f1de690946aeaa14aca
Leonard <andulanleonard@gmail.com>
2023-11-18 12:25:54 +0800

    create printing template for check voucher

---
+++ Controllers/CheckVoucherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting_System.Controllers
+{
+    public class CheckVoucherController : Controller
+    {
+        public IActionResult CheckVoucherIndex()
+        {
+            return View();
+        }
+    }
+}
