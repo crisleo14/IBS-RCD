1ac2455b51977684bb0ddda87dcdba67392ccc1b
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-11-18 14:17:42 +0800

    redesign inventory book and add product code

---
+++ Migrations/20231118061214_add product code in inventory book.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class addproductcodeininventorybook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCode",
+                table: "InventoryBooks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCode",
+                table: "InventoryBooks");
+        }
+    }
+}
