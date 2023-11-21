e8b33198954ca75ef897030b16b190bb7aa634e6
Leonard <andulanleonard@gmail.com>
2023-11-21 11:14:01 +0800

    Add migration for table debit memo need to change name to DMCM

---
+++ Migrations/20231120101223_Create table for Debit Memo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatetableforDebitMemo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DebitMemos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SINo = table.Column<int>(type: "integer", nullable: false),
+                    DMCMNo = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    TransactionDate = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    VatableSales = table.Column<decimal>(type: "numeric", nullable: false),
+                    VatAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    IsPrint = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DebitMemos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DebitMemos_SalesInvoices_SINo",
+                        column: x => x.SINo,
+                        principalTable: "SalesInvoices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DebitMemos_SINo",
+                table: "DebitMemos",
+                column: "SINo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DebitMemos");
+        }
+    }
+}
