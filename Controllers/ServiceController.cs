c234d22290b5893450f4ee2c4784ac71a486c4cb
Gray <121025320+azhadolfo@users.noreply.github.com>
2023-11-21 10:21:08 +0800

    initial create of services master file

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Accounting_System.Data;
+using Accounting_System.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Accounting_System.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ServiceController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Service
+        public async Task<IActionResult> Index()
+        {
+            return _context.Services != null ?
+                        View(await _context.Services.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Services'  is null.");
+        }
+
+        // GET: Service/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var services = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            return View(services);
+        }
+
+        // GET: Service/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Service/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Percent,Id,CreatedBy,CreatedDate")] Services services)
+        {
+            if (ModelState.IsValid)
+            {
+                services.CreatedBy = _userManager.GetUserName(this.User).ToUpper();
+
+                _context.Add(services);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(services);
+        }
+
+        // GET: Service/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var services = await _context.Services.FindAsync(id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+            return View(services);
+        }
+
+        // POST: Service/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Percent,Id,CreatedBy,CreatedDate")] Services services)
+        {
+            if (id != services.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(services);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServicesExists(services.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(services);
+        }
+
+        // GET: Service/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Services == null)
+            {
+                return NotFound();
+            }
+
+            var services = await _context.Services
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (services == null)
+            {
+                return NotFound();
+            }
+
+            return View(services);
+        }
+
+        // POST: Service/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Services == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Services'  is null.");
+            }
+            var services = await _context.Services.FindAsync(id);
+            if (services != null)
+            {
+                _context.Services.Remove(services);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServicesExists(int id)
+        {
+            return (_context.Services?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
