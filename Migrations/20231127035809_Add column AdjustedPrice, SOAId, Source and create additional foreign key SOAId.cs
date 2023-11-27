b5f88e5b79efa634b84481f3118dcd35469c301f
Leonard <andulanleonard@gmail.com>
2023-11-27 15:42:50 +0800

    commit incomplete debit memo module

---
+++ Migrations/20231127035809_Add column AdjustedPrice, SOAId, Source and create additional foreign key SOAId.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddcolumnAdjustedPriceSOAIdSourceandcreateadditionalforeignkeySOAId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AdjustedPrice",
+                table: "DebitMemos",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SOAId",
+                table: "DebitMemos",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Source",
+                table: "DebitMemos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DebitMemos_SOAId",
+                table: "DebitMemos",
+                column: "SOAId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropIndex(
+                name: "IX_DebitMemos_SOAId",
+                table: "DebitMemos");
+
+            migrationBuilder.DropColumn(
+                name: "AdjustedPrice",
+                table: "DebitMemos");
+
+            migrationBuilder.DropColumn(
+                name: "SOAId",
+                table: "DebitMemos");
+
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "DebitMemos");
+        }
+    }
+}
--- Models/DebitMemo.cs
+++ Models/DebitMemo.cs
@@ -6,21 +6,35 @@ namespace Accounting_System.Models
 {
     public class DebitMemo : BaseEntity
     {
+        [Display(Name = "SI No")]
         public int SalesInvoiceId { get; set; }
-
         [ForeignKey("SalesInvoiceId")]
         public SalesInvoice? SalesInvoice { get; set; }
         [NotMapped]
         public List<SelectListItem>? SalesInvoices { get; set; }
 
+
+        [Display(Name = "SOA No")]
+        public int SOAId { get; set; }
+        [ForeignKey("SOAId")]
+        public StatementOfAccount? SOA { get; set; }
+        [NotMapped]
+        public List<SelectListItem>? StatementOfAccounts { get; set; }
+
+
         public string? DMNo { get; set; }
         public DateTime Date {  get; set; }
+        [Display(Name = "Debit Amount")]
         public decimal DebitAmount {  get; set; }
         public string Description { get; set; }
+        [Display(Name = "Vatable Sales")]
         public decimal VatableSales {  get; set; }
         public decimal VatAmount { get; set; }
         public decimal Amount { get; set; }
         public bool IsPrinted { get; set; }
-       
+        [Display(Name = "Adjusted Price")]
+        public decimal AdjustedPrice { get; set; }
+        public string Source { get; set; } = " ";
+
     }
 }
