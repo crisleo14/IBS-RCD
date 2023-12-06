320470830e509d33c3bc46275a09f8742f7ab0cb
Leonard <andulanleonard@gmail.com>
2023-12-06 12:20:44 +0800

    add first model in check voucher create, implement entry and function in controller

--- Controllers/CheckVoucherController.cs
+++ Controllers/CheckVoucherController.cs
@@ -1,7 +1,9 @@
 ﻿using Accounting_System.Data;
+using Accounting_System.Models;
 using Accounting_System.Repository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Accounting_System.Controllers
 {
@@ -25,6 +27,51 @@ namespace Accounting_System.Controllers
             var cv = await _checkVoucherRepo.GetCheckVouchers();
 
             return View(cv);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = new CheckVoucherHeader();
+            viewModel.RR = _dbContext.ReceivingReports
+                .Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.RRNo
+                })
+                .ToList();
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CheckVoucherHeader model)
+        {
+            model.RR = _dbContext.ReceivingReports
+                .Select(s => new SelectListItem
+                {
+                    Value = s.Id.ToString(),
+                    Text = s.RRNo
+                })
+                .ToList();
+
+            if (ModelState.IsValid)
+            {
+                var existingReceivingReport = _dbContext.ReceivingReports
+                                               .FirstOrDefault(si => si.Id == model.RRId);
+
+                    var generateCVNo = await _checkVoucherRepo.GenerateCVNo();
+                    model.SeriesNumber = await _checkVoucherRepo.GetLastSeriesNumberCV();
+                    model.CVNo = generateCVNo;
+                    model.CreatedBy = _userManager.GetUserName(this.User);
+                    _dbContext.Add(model);
+                    await _dbContext.SaveChangesAsync();
+                    TempData["success"] = "Check Voucher created successfully";
+                    return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "The information you submitted is not valid!";
+                return View(model);
+            }
         }
     }
 }
