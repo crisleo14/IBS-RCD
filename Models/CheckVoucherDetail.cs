881b6c1b281c5cdfd5e7dcb726513ce63f7ff27a
Leonard <andulanleonard@gmail.com>
2023-12-06 14:28:57 +0800

    add table check voucher details dbcontext and migration

--- Models/CheckVoucherDetail.cs
+++ Models/CheckVoucherDetail.cs
@@ -1,9 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Accounting_System.Models
 {
     public class CheckVoucherDetail : BaseEntity
     {
+        [Display(Name = "COA No")]
+        public int COAId { get; set; }
+
+        [ForeignKey("COAId")]
+        public ChartOfAccount? ChartOfAccount { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem>? COA { get; set; }
+
         public string AccountNo { get; set; }
         public string AccountName { get; set; }
 
