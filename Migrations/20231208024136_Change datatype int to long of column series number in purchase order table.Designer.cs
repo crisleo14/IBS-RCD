fb87b91ad6e39d66badb248db2e66869e8ebb337
Leonard <andulanleonard@gmail.com>
2023-12-08 15:20:28 +0800

    implement Warning the user after successful added if the series was 100 before to reach the maximum 10 digits SeriesNumber

--- Migrations/20231208024136_Change datatype int to long of column series number in purchase order table.Designer.cs
+++ Migrations/20231208024136_Change datatype int to long of column series number in purchase order table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Accounting_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Accounting_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231208024136_Change datatype int to long of column series number in purchase order table")]
+    partial class Changedatatypeinttolongofcolumnseriesnumberinpurchaseordertable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -269,9 +272,8 @@ namespace Accounting_System.Migrations
                     b.Property<DateTime>("CheckDate")
                         .HasColumnType("timestamp with time zone");
 
-                    b.Property<string>("CheckNo")
-                        .IsRequired()
-                        .HasColumnType("text");
+                    b.Property<int>("CheckNo")
+                        .HasColumnType("integer");
 
                     b.Property<string>("CreatedBy")
                         .HasColumnType("text");
@@ -1134,12 +1136,6 @@ namespace Accounting_System.Migrations
                     b.Property<decimal>("Amount")
                         .HasColumnType("numeric");
 
-                    b.Property<decimal>("AmountPaid")
-                        .HasColumnType("numeric");
-
-                    b.Property<decimal>("Balance")
-                        .HasColumnType("numeric");
-
                     b.Property<string>("BusinessStyle")
                         .IsRequired()
                         .HasColumnType("text");
@@ -1162,9 +1158,6 @@ namespace Accounting_System.Migrations
 
                     b.Property<decimal>("Discount")
                         .HasColumnType("numeric");
-
-                    b.Property<bool>("IsPaid")
-                        .HasColumnType("boolean");
 
                     b.Property<bool>("IsPosted")
                         .HasColumnType("boolean");
