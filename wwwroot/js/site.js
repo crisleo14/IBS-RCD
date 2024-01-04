27faee654c4a7eb07ab607e243d5463d04f94fa6
Leonard <andulanleonard@gmail.com>
2024-01-04 18:35:26 +0800

    Remove decimal in Quantity in SI(branch AR module)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -56,3 +56,68 @@ $(document).ready(function () {
         currentPage = table.page.info().page;
     });
 });
+
+
+//Money Input formatting
+    $(document).ready(function () {
+        // Store the initial input value for each moneyInput field
+        $(".moneyInput").each(function () {
+            $(this).data("initialValue", $(this).val());
+        });
+
+        // Attach a focus event listener to the moneyInput fields
+        $(".moneyInput").on("focus", function () {
+            // Get the initial value
+            var initialValue = $(this).data("initialValue");
+
+            // If the current value is the initial value, clear it
+            if (initialValue === '0.00' || initialValue === '0') {
+                $(this).val('');
+            }
+        });
+
+        // Attach a blur event listener to the moneyInput fields
+        $(".moneyInput").on("blur", function () {
+            // Get the initial value
+            var initialValue = $(this).data("initialValue");
+
+            // If the current value is empty or only whitespace, set it back to the initial value
+            if (!$(this).val().trim()) {
+                $(this).val(initialValue);
+            }
+        });
+
+    // Attach an input event listener to the moneyInput field
+    $(".moneyInput").on("input", function () {
+                    // Get the current value of the input
+                    var inputValue = $(this).val();
+
+    // If the input starts with a dot, add '0' before it
+    if (inputValue.charAt(0) === '.') {
+        inputValue = '0' + inputValue;
+                    }
+
+    // Remove non-numeric characters except for the decimal point
+    var numericValue = inputValue.replace(/[^\d.]/g, '');
+
+    // Format the numeric value with commas and two decimal places
+    var formattedValue = numberWithCommasAndDecimals(numericValue);
+
+    // Set the formatted value back to the input field
+    $(this).val(formattedValue);
+                });
+
+    // Function to add commas and two decimal places to a number
+    function numberWithCommasAndDecimals(number) {
+                    // Split the number into integer and decimal parts
+                    var parts = number.split('.');
+    var integerPart = parts[0];
+                    var decimalPart = parts.length > 1 ? '.' + parts[1] : '';
+
+    // Add commas to the integer part
+    var numberWithCommas = integerPart.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    // Combine the integer and decimal parts
+    return numberWithCommas + decimalPart;
+                }
+            });
