5b9cbbf78ac051e86575573225ad65a968d85022
Gray <121025320+azhadolfo@users.noreply.github.com>
2024-01-08 15:47:03 +0800

    remove original copy

--- Models/SalesInvoice.cs
+++ Models/SalesInvoice.cs
@@ -91,8 +91,6 @@ namespace Accounting_System.Models
         [Column(TypeName = "varchar(100)")]
         public string Remarks { get; set; }
 
-        public bool OriginalCopy { get; set; } = true;
-
         [Display(Name = "Vatable Sales")]
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
         public decimal VatableSales { get; set; }
