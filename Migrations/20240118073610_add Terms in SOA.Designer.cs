145829a1b2ef297eb316fddaa16db191e62c66f8
Gray <121025320+azhadolfo@users.noreply.github.com>
2024-01-18 16:53:05 +0800

    add new field in soa and implement the prior terms

--- Migrations/20240118073610_add Terms in SOA.Designer.cs
+++ Migrations/20240118073610_add Terms in SOA.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Accounting_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Accounting_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240118073610_add Terms in SOA")]
+    partial class addTermsinSOA
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1730,7 +1733,7 @@ namespace Accounting_System.Migrations
 
                     b.Property<string>("Terms")
                         .IsRequired()
-                        .HasColumnType("varchar(5)");
+                        .HasColumnType("text");
 
                     b.Property<decimal>("Total")
                         .HasColumnType("numeric(18,2)");
