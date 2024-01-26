fc0c3b0a48fad5d51f34f35a6a45d4cf59669a49
Gray <121025320+azhadolfo@users.noreply.github.com>
2024-01-26 13:45:12 +0800

    [200~Remove terms and change to Due Date and in printing terms will be blank

---
+++ Migrations/20240126053447_remove terms field and add due date field.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class removetermsfieldandaddduedatefield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Terms",
+                table: "StatementOfAccounts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "StatementOfAccounts",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "StatementOfAccounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Terms",
+                table: "StatementOfAccounts",
+                type: "varchar(5)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
