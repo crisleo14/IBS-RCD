3d2fce1a7c67b957162c2c85bb7938b0341b922e
Leonard <andulanleonard@gmail.com>
2024-02-19 18:45:13 +0800

    Done create SOA entry in DM with Migration and Revise Post SOA in GL and SalesBook

--- Migrations/20240219091849_Add new fields in DM name Period, Amount, currentandPreviousAmount, UnearnedAmount.Designer.cs
+++ Migrations/20240219091849_Add new fields in DM name Period, Amount, currentandPreviousAmount, UnearnedAmount.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Accounting_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Accounting_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240219091849_Add new fields in DM name Period, Amount, currentandPreviousAmount, UnearnedAmount")]
+    partial class AddnewfieldsinDMnamePeriodAmountcurrentandPreviousAmountUnearnedAmount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
