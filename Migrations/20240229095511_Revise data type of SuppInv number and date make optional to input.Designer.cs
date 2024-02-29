06378600553014fff0ecb82d1a7a5cbd68635c7f
Leonard <andulanleonard@gmail.com>
2024-02-29 18:04:27 +0800

    Suppier Invoice # and Supplier date is optional.

--- Migrations/20240229095511_Revise data type of SuppInv number and date make optional to input.Designer.cs
+++ Migrations/20240229095511_Revise data type of SuppInv number and date make optional to input.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Accounting_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Accounting_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240229095511_Revise data type of SuppInv number and date make optional to input")]
+    partial class RevisedatatypeofSuppInvnumberanddatemakeoptionaltoinput
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
