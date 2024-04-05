2c4c2dfcbea1bd1f759f898aab88e04898073b7c
crisleo14 <andulanleonard@gmail.com>
2024-04-05 13:59:41 +0800

    Revise create entry of CV incompleter proccess

--- Migrations/20240405043002_Add new field SINo and change data type of RRNo in CV.Designer.cs
+++ Migrations/20240405043002_Add new field SINo and change data type of RRNo in CV.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Accounting_System.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Accounting_System.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240405043002_Add new field SINo and change data type of RRNo in CV")]
+    partial class AddnewfieldSINoandchangedatatypeofRRNoinCV
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -275,6 +278,7 @@ namespace Accounting_System.Migrations
                         .HasColumnType("boolean");
 
                     b.Property<string[]>("PONo")
+                        .IsRequired()
                         .HasColumnType("varchar[]");
 
                     b.Property<string>("Particulars")
