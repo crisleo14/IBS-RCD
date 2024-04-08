5a9493d48ed17305efdadf699ed7f187b85b448a
crisleo14 <andulanleonard@gmail.com>
2024-04-08 18:28:58 +0800

    Revise model of CV remove column Bank and add new column BankId and foreign key into BankAccounts

---
+++ Migrations/20240408101255_Remove column field named Bank in CV.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounting_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovecolumnfieldnamedBankinCV : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bank",
+                table: "CheckVoucherHeaders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bank",
+                table: "CheckVoucherHeaders",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
