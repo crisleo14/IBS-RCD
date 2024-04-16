521f407832e8fd915cefad98a1cc21d24f35cc52
crisleo14 <andulanleonard@gmail.com>
2024-04-16 12:12:04 +0800

    Revise validation of input of Check Voucher No. CV module

--- Models/CheckVoucherHeader.cs
+++ Models/CheckVoucherHeader.cs
@@ -55,7 +55,7 @@ namespace Accounting_System.Models
 
         [Display(Name = "Check #")]
         [Required(ErrorMessage = "The field is required")]
-        [RegularExpression(@"^(?:\d+|DM\d{10})$", ErrorMessage = "Invalid format. Please enter either a CV number or 'DM' followed by a 10-digit number.")]
+        [RegularExpression(@"^(?:\d{10,}|DM\d{10})$", ErrorMessage = "Invalid format. Please enter either a 'DM' followed by a 10-digits or CV number minimum 10 digits.")]
         public string CheckNo { get; set; }
 
         public string Category { get; set; }
