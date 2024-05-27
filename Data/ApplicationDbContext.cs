4ba916d95a377fb5d2d7e0929cf2c4a46201a6e1
azh <azhadolfo1@gmail.com>
2024-05-27 18:04:01 +0800

    changes of layout and design

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,7 +13,6 @@ namespace Accounting_System.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<SalesInvoice> SalesInvoices { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<Ledger> Ledgers { get; set; }
         public DbSet<ChartOfAccount> ChartOfAccounts { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<AuditTrail> AuditTrails { get; set; }
