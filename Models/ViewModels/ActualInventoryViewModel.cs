54addcad5d38c020a96814f57b201c0e79fc640f
crisleo14 <andulanleonard@gmail.com>
2024-06-06 11:18:12 +0800

    Create Actual Inventory module without (GL entries)

---
+++ Models/ViewModels/ActualInventoryViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Accounting_System.Models.ViewModels
+{
+    public class ActualInventoryViewModel
+    {
+        [Required]
+        public DateOnly Date { get; set; }
+
+        [Required(ErrorMessage = "The product field is required")]
+        public int ProductId { get; set; }
+
+        public List<SelectListItem>? ProductList { get; set; }
+
+        [Display(Name = "Per Book")]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal PerBook { get; set; }
+
+        [Display(Name = "Per Book")]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal AverageCost { get; set; }
+
+        [Display(Name = "Per Book")]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal TotalBalance { get; set; }
+
+        [Required(ErrorMessage = "Cost must be greater than zero")]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Actual Amount")]
+        public decimal ActualAmount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = true)]
+        public decimal Variance { get; set; }
+    }
+}
