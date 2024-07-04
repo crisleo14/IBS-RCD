18d658abce102c00f1c173c656796f0208b4375e
crisleo14 <andulanleonard@gmail.com>
2024-07-04 14:10:24 +0800

    Revise CV invoicing edit module

--- Controllers/CheckVoucherController.cs
+++ Controllers/CheckVoucherController.cs
@@ -1431,21 +1431,64 @@ namespace Accounting_System.Controllers
                     #region --CV Details Entry
 
                     var existingDetailsModel = await _dbContext.CheckVoucherDetails.Where(d => d.TransactionNo == existingModel.CVNo).ToListAsync();
-                    CheckVoucherDetail detailsModel = new();
 
-                    var cashInBank = 0m;
-                    for (int i = 0; i < existingDetailsModel.Count(); i++)
+                    // Dictionary to keep track of AccountNo and their ids for comparison
+                    var accountTitleDict = new Dictionary<string, List<int>>();
+                    foreach (var details in existingDetailsModel)
                     {
-                        var cvd = existingDetailsModel[i];
-                        cvd.AccountNo = viewModel.AccountNumber[i];
-                        cvd.AccountName = viewModel.AccountTitle[i];
-                        cvd.Debit = viewModel.Debit[i];
-                        cvd.Credit = viewModel.Credit[i];
-                        cvd.TransactionNo = existingModel.CVNo;
+                        if (!accountTitleDict.ContainsKey(details.AccountNo))
+                        {
+                            accountTitleDict[details.AccountNo] = new List<int>();
+                        }
+                        accountTitleDict[details.AccountNo].Add(details.Id);
                     }
 
-                    var newDetailsModel = new List<CheckVoucherDetail>(); // Replace with the actual new details
-                    existingDetailsModel.AddRange(newDetailsModel);
+                    // Add or update records
+                    for (int i = 0; i < viewModel.AccountTitle.Length; i++)
+                    {
+
+                        if (accountTitleDict.TryGetValue(viewModel.AccountNumber[i], out var ids))
+                        {
+                            // Update the first matching record and remove it from the list
+                            var detailsId = ids.First();
+                            ids.RemoveAt(0);
+                            var details = existingDetailsModel.First(o => o.Id == detailsId);
+
+                            details.AccountNo = viewModel.AccountNumber[i];
+                            details.AccountName = viewModel.AccountTitle[i];
+                            details.Debit = viewModel.Debit[i];
+                            details.Credit = viewModel.Credit[i];
+                            details.TransactionNo = existingModel.CVNo;
+
+                            if (ids.Count == 0)
+                            {
+                                accountTitleDict.Remove(viewModel.AccountNumber[i]);
+                            }
+                        }
+                        else
+                        {
+                            // Add new record
+                            var newDetails = new CheckVoucherDetail
+                            {
+                                AccountNo = viewModel.AccountNumber[i],
+                                AccountName = viewModel.AccountTitle[i],
+                                Debit = viewModel.Debit[i],
+                                Credit = viewModel.Credit[i],
+                                TransactionNo = existingModel.CVNo
+                            };
+                            _dbContext.CheckVoucherDetails.Add(newDetails);
+                        }
+                    }
+
+                    // Remove remaining records that were duplicates
+                    foreach (var ids in accountTitleDict.Values)
+                    {
+                        foreach (var id in ids)
+                        {
+                            var details = existingDetailsModel.First(o => o.Id == id);
+                            _dbContext.CheckVoucherDetails.Remove(details);
+                        }
+                    }
 
                     #endregion --CV Details Entry
 
